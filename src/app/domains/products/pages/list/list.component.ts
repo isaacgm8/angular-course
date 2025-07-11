33ddf72529ca5a521d533b20d8b04cfdd7c85786
isaacb <morenoisaac596@gmail.com>
2025-07-10 22:38:16 -0500

    class 22

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,78 +1,48 @@
-import {
-  Component,
-  Input,
-  SimpleChanges,
-  inject,
-  signal,
-  OnInit,
-  OnChanges,
-} from '@angular/core';
+import { Component, inject, input, resource} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLinkWithHref } from '@angular/router';
 import { ProductComponent } from '@products/components/product/product.component';
-import { ActivatedRoute } from '@angular/router';
 
 import { Product } from '@shared/models/product.model';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
 import { CategoryService } from '@shared/services/category.service';
-import { Category } from '@shared/models/category.model';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-list',
   imports: [CommonModule, ProductComponent, RouterLinkWithHref],
   templateUrl: './list.component.html',
 })
-export default class ListComponent implements OnInit, OnChanges {
-  products = signal<Product[]>([]);
-  categories = signal<Category[]>([]);
+export default class ListComponent{
   private cartService = inject(CartService);
   private productService = inject(ProductService);
   private categoryService = inject(CategoryService);
-    private route = inject(ActivatedRoute);
-   slug: string | null = null;
+  readonly slug = input<string>();
 
-     ngOnChanges(changes: SimpleChanges) {
-    if (changes['category_id']) {
-      this.getProducts();
-    }
-  }
 
- ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      this.slug = params.get('slug');
-       this.getProducts(); 
-    });
-    this.getCategories();
-  }
+  categoriesResource = resource({
+    loader: () => this.categoryService.getAllPromise()
+  })
+
+  productsResource = rxResource({
+    request: () => ({ category_slug: this.slug() }),
+    loader: ({ request }) => this.productService.getProducts(request),
+  })
     
   addToCart(product: Product) {
     this.cartService.addToCart(product);
   }
 
-  private getProducts() { 
-     const params: any = {};
-      if (this.slug) {
-        params.category_slug = this.slug;
-  }
-    this.productService.getProducts(params).subscribe({
-      next: (products) => {
-        this.products.set(products);
-      },
-      error: (err) => {
-        console.error('Error fetching products:', err);
-      },
-    });
+
+  resetCategories() {
+    this.categoriesResource.set([]);
   }
 
-  private getCategories() {
-    this.categoryService.getAll().subscribe({
-      next: (data) => {
-        this.categories.set(data);
-      },
-      error: (err) => {
-        console.error('Error fetching products:', err);
-      },
-    });
+  reloadCtegories() {
+    this.categoriesResource.reload(); 
+  }
+  reloadProducts() {
+    this.productsResource.reload()
   }
 }
